67208797b63cb1117e1be00ebef1f97721f23216
Suvorov Daniil <severecloud@gmail.com>
2020-07-25 22:23:10 +0300

    add UsersStack

---
+++ src/hooks/useBrowserInfo.ts
@@ -0,0 +1,15 @@
+import { getContext } from 'svelte';
+import { SSRContextKey, SSRContextInterface } from '../lib/ssr';
+import { canUseDOM } from '../lib/dom';
+import { BrowserInfo, computeBrowserInfo } from '../lib/browser';
+
+export default function useBrowserInfo(): BrowserInfo {
+    const ssrContext = getContext(SSRContextKey) as SSRContextInterface;
+    
+    let userAgent = (canUseDOM && navigator.userAgent ? navigator.userAgent : '')
+    if (ssrContext && ssrContext.platform) {
+        userAgent =  ssrContext.platform;
+    }
+
+    return computeBrowserInfo(userAgent);
+  }
--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,10 @@
 export type FormStatusType = 'default' | 'error' | 'valid';
 
 export type AlignType = 'left' | 'center' | 'right';
+
+
+export interface Version {
+    major: number;
+    minor?: number;
+    patch?: number;
+  }
