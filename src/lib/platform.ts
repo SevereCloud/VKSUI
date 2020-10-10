a620c6775590a77dddad22ce03c36cca9305e1d3
Suvorov Daniil <severecloud@gmail.com>
2020-10-10 21:00:23 +0300

    feat: add vkcom platform

--- src/lib/platform.ts
+++ src/lib/platform.ts
@@ -3,10 +3,12 @@ import { canUseDOM } from './dom';
 export enum OS {
   ANDROID = 'android',
   IOS = 'ios',
+  VKCOM = 'vkcom'
 }
 
 export const ANDROID: OS = OS.ANDROID;
 export const IOS: OS = OS.IOS;
+export const VKCOM: OS = OS.VKCOM;
 
 export function platform(useragent?: string): OS {
   const ua = useragent || (canUseDOM && navigator.userAgent) || '';
